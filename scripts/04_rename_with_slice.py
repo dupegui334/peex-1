a96df2e573f84f2ccd54f83a33e58556acf83c0c
David Upegui <davidupegui334@hotmail.com>
2023-05-08 20:35:54 -0500

    Setting inputs with sys.argv

--- scripts/04_rename_with_slice.py
+++ scripts/04_rename_with_slice.py
@@ -3,14 +3,14 @@ import glob
 import sys
 
 os.chdir(sys.argv[1])
-print(os.getcwd())
+print("Current path " + os.getcwd())
 for file in glob.glob(f"*.{sys.argv[2]}"):
     file_name = os.path.splitext(file)[0]
     extension = os.path.splitext(file)[1]
-    new_file_name = file_name[:-sys.argv[3]] + extension
+    new_file_name = file_name[:-int(sys.argv[3])] + extension
     try:
         os.rename(file, new_file_name)
     except OSError as e:
         print(e)
     else:
-        print("Renamed {} to {}".format(file, new_file_name))
+        print(f"Renamed {file} to {new_file_name}")
