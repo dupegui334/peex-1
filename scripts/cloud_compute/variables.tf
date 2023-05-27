5bdd63e3eb04344695eca94e1bc527ef80a50403
dupeguib@cisco.com <dupeguib@cisco.com>
2023-05-26 20:00:19 -0500

    Added terraform parent file for the infrastructure

---
+++ scripts/cloud_compute/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr_block" {
+  default     = "10.0.0.0/16"
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnet_cidr_block" {
+  type    = list(string)
+  default = ["10.0.0.0/24"]
+}
+
+variable "ami" {
+    default = "ami-0715c1897453cabd1"
+}
+
+variable "instance-type" {
+    default = "t2.micro"
+}
+
