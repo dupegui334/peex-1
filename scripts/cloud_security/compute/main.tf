936eec872f8ca5a85aed2d175726944a896ef966
dupegui334 <davidupegui334@hotmail.com>
2023-06-04 17:36:07 -0500

    Created terraform files for compute resources

---
+++ scripts/cloud_security/compute/main.tf
@@ -0,0 +1,124 @@
+resource "aws_instance" "ec2-priv" {
+  ami           = var.ami
+  instance_type = var.instance-type
+  key_name               = data.aws_key_pair.ec2-key.key_name
+  vpc_security_group_ids = [aws_security_group.vm-sg-priv.id]
+  subnet_id = var.private_subnet[0] #Need to associate to subnet created so we can create SG in the VPC
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "VM1-priv"
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami           = var.ami
+  instance_type = var.instance-type
+  key_name               = data.aws_key_pair.ec2-key.key_name
+  vpc_security_group_ids = [aws_security_group.sg-bastion.id]
+  subnet_id = var.public_subnet[0] #Need to associate to subnet created so we can create SG in the VPC
+  associate_public_ip_address = true
+  iam_instance_profile = aws_iam_instance_profile.ssm-instance-role.name
+
+
+  tags = {
+    Name = "bastion-host"
+  }
+}
+
+resource "aws_iam_user" "softserve-user" { 
+    name = "bastion-user"
+    tags = {
+        Description = "User used to access EC2 for Peex"
+    }
+}
+
+resource "aws_iam_role" "ssm-ec2-role" {
+    name = "SSMFullAccess"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action : "sts:AssumeRole",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                Effect   = "Allow"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ssm-ec2-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
+
+resource "aws_iam_instance_profile" "ssm-instance-role" {
+  name  = "ssm-instance-role"
+  role = aws_iam_role.ssm-ec2-role.name
+}
+
+data "aws_key_pair" "ec2-key" {
+  key_name = "centos-ec2"
+}
+
+resource "aws_security_group" "sg-bastion" {
+  name        = "bastion-sg"
+  description = "Security group for bastion"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH connection"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https connection"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_security_group" "vm-sg-priv" {
+  name        = "VM-sg-priv"
+  description = "Security group for VM priv"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH connection"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/17"] #Allow public VM in public subnet of the VPC to ssh it.
+  }
+
+  ingress {
+    description = "ICMP" #All ICMP
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["10.0.0.0/17"] #Allow public VM in public subnet of the VPC to ping it.
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
